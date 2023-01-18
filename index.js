6e15ce30a3fe3b0f3e99bf935aff388d02a82a15
Vanessa Chan <vchan@Vanessas-MacBook-Air.local>
2023-01-17 22:11:40 -0800

    update

--- index.js
+++ index.js
@@ -13,3 +13,40 @@ const mainHtmlFile = require("./src/main-html");
 const teamArray = [];
 let htmlFile = "";
 
+// Manager Fx questions
+mgrQuestions = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "mgrname",
+        message: "Enter name of team manager:",
+      },
+      {
+        type: "input",
+        name: "mgrid",
+        message: "Enter manager ID #:",
+      },
+      {
+        type: "input",
+        name: "mgremail",
+        message: "Enter manager email:",
+      },
+      {
+        type: "input",
+        name: "mgrofficenum",
+        message: "Enter manager office #:",
+      },
+    ])
+    .then((response) => {
+      const manager = new Manager(
+        response.mgrname,
+        response.mgrid,
+        response.mgremail,
+        response.mgrofficenum
+      );
+      teamArray.push(manager);
+      optionQuestion();
+    });
+};
+
